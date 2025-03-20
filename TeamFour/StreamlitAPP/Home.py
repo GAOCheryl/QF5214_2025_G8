f03aa122bd682f35ecf0db9d45c908710da588be
clairehur <143524836+clairehur@users.noreply.github.com>
2025-03-20 23:45:27 +0900

    Updated sidebar and page titles

---
+++ TeamFour/StreamlitAPP/Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Stock Analysis App", page_icon="📈", layout="wide")
+
+# Display Group Name in small font at top left
+st.markdown("<h4 style='text-align: left; font-size: 14px;'>QF5214 Group 8</h4>", unsafe_allow_html=True)
+
+# Display Main Header
+st.markdown("<h1 style='text-align: center;'>From Sentiment to Strategy: Integrating NLP-Derived Factors into Quantitative Investment Models</h1>", unsafe_allow_html=True)
+
+# Display Group Members in smaller italic font
+st.markdown("""
+*Group Members: Gao XuanRong, Hur Sinhaeng, Li LingYan, Liu Yang, Ren ZhiNan, Zhang YiChen, Zhou Zheng, Zhang Leyan*
+""", unsafe_allow_html=True)
---
+++ TeamFour/StreamlitAPP/pages/Sentiment & Stock Performance.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.set_page_config(page_title="Sentiment & Stock Performance")
+st.title("Sentiment & Stock Performance Relation")
