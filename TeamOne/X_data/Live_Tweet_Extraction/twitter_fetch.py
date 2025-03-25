d629239310e4e6dc001296fb1874080c80dc0b03
clairehur <143524836+clairehur@users.noreply.github.com>
2025-03-25 20:54:49 +0900

    live tweet

--- TeamOne/X_data/Live_Tweet_Extraction/twitter_fetch.py
+++ TeamOne/X_data/Live_Tweet_Extraction/twitter_fetch.py
@@ -10,3 +10,13 @@ print("🚀 All comapnies 50 tweets Final")
 API_KEY = "93af433a8ff843819d702acbfadcc895"
 base_url = "https://api.twitterapi.io/twitter/tweet/advanced_search"
 headers = {"X-API-Key": API_KEY}
+
+# List of tickers
+tickers = [
+    "ADBE", "AMD", "ABNB", "GOOGL", "GOOG", "AMZN", "AEP", "AMGN", 
+    "ADI", "ANSS", "AAPL", "AMAT", "APP", "ARM", "ASML", "AZN", 
+    "TEAM", "ADSK", "ADP", "AXON", "BKR", "BIIB", "BKNG", "AVGO", 
+    "CDNS", "CDW", "CHTR", "CTAS", "CSCO", "CCEP", "CTSH", "CMCSA", 
+    "CEG", "CPRT", "CSGP", "COST", "CRWD", "CSX", "DDOG", "DXCM", 
+    "VRTX", "WBD", "WDAY", "XEL", "ZS", "QCOM", "REGN", "ROP", "ROST"
+]
