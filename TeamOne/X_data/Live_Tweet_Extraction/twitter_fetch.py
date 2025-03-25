0c82099f14bc10cdff0f59cca103c1ca74f83170
clairehur <143524836+clairehur@users.noreply.github.com>
2025-03-25 20:54:15 +0900

    live tweet

--- TeamOne/X_data/Live_Tweet_Extraction/twitter_fetch.py
+++ TeamOne/X_data/Live_Tweet_Extraction/twitter_fetch.py
@@ -5,3 +5,8 @@ from datetime import datetime
 import psycopg2
 
 print("🚀 All comapnies 50 tweets Final")
+
+# Twitter API config
+API_KEY = "93af433a8ff843819d702acbfadcc895"
+base_url = "https://api.twitterapi.io/twitter/tweet/advanced_search"
+headers = {"X-API-Key": API_KEY}
