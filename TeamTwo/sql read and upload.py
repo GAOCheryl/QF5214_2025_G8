e439d5620548a22a7ff20245f46d2f62d6ef7b13
zhang-lyy <zhang_leyan@126.com>
2025-03-31 15:47:56 +0800

    Update sql read and upload.py

--- TeamTwo/sql read and upload.py
+++ TeamTwo/sql read and upload.py
@@ -4,16 +4,13 @@ from datetime import datetime
 
 analyzer = SentimentEmotionAnalyzer()
 
-# PostgreSQL connection settings
 db_user = "postgres"
 db_password = "qf5214"
 db_host = "134.122.167.14"
 db_port = 5555
 db_name = "QF5214"
 
-# Create engine
 engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
-
 target_companies = ["ADP"]
 
 for target_company in target_companies:
